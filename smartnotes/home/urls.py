7ea6025f015aca753aa8e19f5fda55f86f1c60a0
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2023-12-20 12:40:45 +0530

    .

--- smartnotes/home/urls.py
+++ smartnotes/home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    
     path('home',views.HomeView.as_view()),#changed according to class view
     path('authorize',views.AuthorizedView.as_view()),
 ]
--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
 from . models import Notes
 from django.http import Http404
-from django.views.generic import ListView
+from django.views.generic import DetailView,ListView
 
-class NotesListVieew(ListView):
+class NotesListView(ListView):
+    # use cheyyunna model
     model = Notes
+
+    # pass cheyyunna key name
     context_object_name = "notes"
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request,'notes/notes_list.html',{'notes': all_notes})
+    # use cheyyunna template
+    template_name = 'notes/notes_list.html'
 
-def detail(request, pk):
-    try:
-        notes = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        return render(request,'notes/error.html',{})
-        #raise Http404("Note does not exist")
-    return render(request, 'notes/notes_details.html', {'note':notes})
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request,'notes/notes_list.html',{'notes': all_notes})
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    template_name = 'notes/notes_details.html'
+
+
+# def detail(request, pk):
+#     try:
+#         notes = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         return render(request,'notes/error.html',{})
+#         #raise Http404("Note does not exist")
+#     return render(request, 'notes/notes_details.html', {'note':notes})
