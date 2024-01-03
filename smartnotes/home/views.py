1ab54cefc8aae0c02973f4b0bea2cddd3d99c086
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-01-03 12:34:56 +0530

    .

--- smartnotes/home/views.py
+++ smartnotes/home/views.py
@@ -28,7 +28,7 @@ class AuthorizedView(LoginRequiredMixin, TemplateView):
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
 
-class LogoutInterfaceVie(LogoutView):
+class LogoutInterfaceView(LogoutView):
     pass
     #template_name = 'home/logout.html'
 
